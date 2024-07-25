ca1bb3e312fb5044a0e24b839367cd1044befb3f
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2024-07-25 13:53:59 -0400

    small fix

--- src/consts/configs.ts
+++ src/consts/configs.ts
@@ -2,7 +2,6 @@ export const appConfigs = {
   port: process.env.PORT || 8080,
   adminEmail: process.env.ADMIN_EMAIL || 'admin@mail.mock',
   jwtSecret: process.env.JWT_SECRET || 'jwt',
-  placesLimit: 100,
   database: {
     host: process.env.DATABASE_HOST || 'localhost',
     user: process.env.DATABASE_USER || 'root',
