04d0ea63f235cbf192f7110bbab16295eb7903cd
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2024-07-31 11:17:33 -0400

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import 'reflect-metadata'
 
 import express, { Express } from 'express'
@@ -6,6 +7,7 @@ import { auth } from './api/middlewares'
 import { router } from './api/routes'
 
 const app: Express = express()
+app.use(cors())
 app.use(express.json())
 app.use(auth)
 app.use(router)
